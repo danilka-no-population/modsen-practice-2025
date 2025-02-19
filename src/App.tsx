0118658768f42338e2e97dff432af7514e15886b
Daniil Yazvinski <daniil.yazvinskiy@gmail.com>
2025-02-20 01:37:53 +0300

    feat: column component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import Column from './components/Column/Column';
+import { BoardWrapper } from './components/Column/styled';
 import Header from './components/Header/Header';
 import GlobalStyles from './styles/globalStyles';
 
@@ -6,6 +8,11 @@ function App() {
         <>
             <GlobalStyles />
             <Header />
+            <BoardWrapper>
+                <Column title={'To Do'} color="#4F46E5" taskCount={2} />
+                <Column title={'In progress'} color="#F59E0B" taskCount={3} />
+                <Column title={'Done'} color="#22C55E" taskCount={2} />
+            </BoardWrapper>
         </>
     );
 }
