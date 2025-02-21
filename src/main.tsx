c6ef30ccea0d57a0209ed74fa64cb3052dfdff54
Daniil Yazvinski <daniil.yazvinskiy@gmail.com>
2025-02-21 15:53:55 +0300

    feat: created store provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import App from './App.tsx';
+import { store } from './store/store.ts';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </StrictMode>
 );
